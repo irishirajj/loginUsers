3085f135eaab9467d97601b45d050462d9e1b442
Rishi <rishiraj.mec18@itbhu.ac.in>
2023-03-26 11:50:56 +0530

    Completed the three functionalities

---
+++ WebApplication1/WebApplication1/Login.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WebApplication1.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox runat="server"></asp:TextBox>
+            <asp:TextBox runat="server"></asp:TextBox>
+            <asp:Button runat="server" Text="Login" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/Login.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // Check if the user entered valid credentials
+            if (IsValidUser(username, password))
+            {
+                // Redirect the user to the main page
+                Response.Redirect("Main.aspx");
+            }
+            else
+            {
+                // Display an error message
+                lblError.Text = "Invalid username or password.";
+            }
+        }
+        private bool IsValidUser(string username, string password)
+        {
+            // TODO: Check if the user is valid using your authentication logic
+            // For example, you could check if the username and password match a user in the database
+            // or authenticate against an external identity provider.
+            // Return true if the user is valid, false otherwise.
+        }
+    }
+}
